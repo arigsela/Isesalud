7031b536d020a1291fcb23afc393a3c207ebef3b
Jesus Espinoza Hernandez <chuyespinoza@gmail.com>
2013-02-13 17:52:26 -0800

    Created DbMessages enumeration.

---
+++ ExpedienteWeb/src/main/java/com/isesalud/support/components/DbMessages.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.isesalud.support.components;
+
+/**
+ * @author Jesus Espinoza Hernandez
+ *
+ */
+public enum DbMessages
+{
+	RECORD_NOT_FOUND,
+	RECORD_NOT_UPDATED,
+	RECORD_NOT_DELETED;
+}
