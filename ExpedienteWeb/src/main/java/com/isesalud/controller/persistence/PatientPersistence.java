24686e10c5f4c22a79190112394753509b1809bf
ari <arigsela@gmail.com>
2013-02-14 09:47:24 -0800

    bleh error

--- ExpedienteWeb/src/main/java/com/isesalud/controller/persistence/PatientPersistence.java
+++ ExpedienteWeb/src/main/java/com/isesalud/controller/persistence/PatientPersistence.java
@@ -7,7 +7,6 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
