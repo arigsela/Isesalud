2afd96caf27b4dd0860d10c1c98e541ef7393aa3
Jesus Espinoza Hernandez <chuyespinoza@gmail.com>
2013-02-18 18:00:26 -0800

    Added the rest of the one to many fields. Updated home page to display more fields.

--- ExpedienteWeb/src/main/java/com/isesalud/controller/persistence/PatientPersistence.java
+++ ExpedienteWeb/src/main/java/com/isesalud/controller/persistence/PatientPersistence.java
@@ -3,20 +3,32 @@
  */
 package com.isesalud.controller.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.bean.ViewScoped;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.faces.event.ActionEvent;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.isesalud.ejb.persistence.PatientPersistenceEjb;
+import com.isesalud.model.CancerOtrasPartes;
 import com.isesalud.model.Paciente;
+import com.isesalud.model.ParienteCancerMama;
+import com.isesalud.model.PreviaEnfermedad;
+import com.isesalud.model.SintomaCancerMama;
 import com.isesalud.support.components.BaseManagedCrudController;
+import com.isesalud.support.exceptions.BaseException;
 
 /**
  * @author Jesus Espinoza Hernandez
  *
  */
 @Named
-@ViewScoped
+@ConversationScoped
 public class PatientPersistence extends BaseManagedCrudController<Paciente, PatientPersistenceEjb> {
 
 	/**
@@ -26,6 +38,158 @@ public class PatientPersistence extends BaseManagedCrudController<Paciente, Pati
 	
 	@EJB
 	private PatientPersistenceEjb manager;
+	
+	@Inject
+	private Conversation conversation;
+	
+	/**************CHILD LIST*******************************************/
+	private PreviaEnfermedad previaEnfermedad;
+	private CancerOtrasPartes cancerOtrasPartes;
+	private ParienteCancerMama parienteCancerMama;
+	private SintomaCancerMama sintomaCancerMama;
+	
+	private List<PreviaEnfermedad> prevEnfList = new ArrayList<PreviaEnfermedad>();
+	private List<CancerOtrasPartes> cancerList = new ArrayList<CancerOtrasPartes>();
+	private List<ParienteCancerMama> parienteList = new ArrayList<ParienteCancerMama>();
+	private List<SintomaCancerMama> sintomaList = new ArrayList<SintomaCancerMama>();
+	
+	public PreviaEnfermedad getPreviaEnfermedad() {
+		return previaEnfermedad;
+	}
+	
+	public void setPreviaEnfermedad(PreviaEnfermedad previaEnfermedad) {
+		this.previaEnfermedad = previaEnfermedad;
+	}
+	
+	public CancerOtrasPartes getCancerOtrasPartes() {
+		return cancerOtrasPartes;
+	}
+	
+	public void setCancerOtrasPartes(CancerOtrasPartes cancerOtrasPartes) {
+		this.cancerOtrasPartes = cancerOtrasPartes;
+	}
+	
+	public ParienteCancerMama getParienteCancerMama() {
+		return parienteCancerMama;
+	}
+	
+	public void setParienteCancerMama(ParienteCancerMama parienteCancerMama) {
+		this.parienteCancerMama = parienteCancerMama;
+	}
+	
+	public List<PreviaEnfermedad> getPrevEnfList() {
+		return prevEnfList;
+	}
+	
+	public SintomaCancerMama getSintomaCancerMama() {
+		return sintomaCancerMama;
+	}
+	
+	public void setSintomaCancerMama(SintomaCancerMama sintomaCancerMama) {
+		this.sintomaCancerMama = sintomaCancerMama;
+	}
+	
+	public void setPrevEnfList(List<PreviaEnfermedad> prevEnfList) {
+		this.prevEnfList = prevEnfList;
+	}
+	
+	public List<CancerOtrasPartes> getCancerList() {
+		return cancerList;
+	}
+	
+	public void setCancerList(List<CancerOtrasPartes> cancerList) {
+		this.cancerList = cancerList;
+	}
+	
+	public List<ParienteCancerMama> getParienteList() {
+		return parienteList;
+	}
+	
+	public void setParienteList(List<ParienteCancerMama> parienteList) {
+		this.parienteList = parienteList;
+	}
+	
+	public List<SintomaCancerMama> getSintomaList() {
+		return sintomaList;
+	}
+	
+	public void setSintomaList(List<SintomaCancerMama> sintomaList) {
+		this.sintomaList = sintomaList;
+	}
+	/*************END CHILD LIST*****************************************/
+	
+	
+	@PostConstruct
+	public void init(){
+		if(conversation != null && conversation.isTransient()){
+			this.parienteCancerMama = new ParienteCancerMama();
+			this.cancerOtrasPartes = new CancerOtrasPartes();
+			this.previaEnfermedad = new PreviaEnfermedad();
+			this.sintomaCancerMama = new SintomaCancerMama();
+		}
+	}
+	
+	@Override
+	protected void doBeforeSave() throws BaseException {
+		super.doBeforeSave();
+		getModel().setParientescancermama(parienteList);
+		getModel().setCanceresOtraPartes(cancerList);
+		getModel().setPreviasenfermedades(prevEnfList);
+		getModel().setSintomasCancerMama(sintomaList);
+	}
+	
+	@Override
+	protected void doAfterAdd() throws BaseException {
+		if(conversation.isTransient())
+			conversation.begin();
+		super.doAfterAdd();
+	}
+	
+	@Override
+	protected void doAfterSave() throws BaseException {
+		super.doAfterSave();
+		if(!conversation.isTransient())
+			conversation.end();
+	}
+	
+	@Override
+	protected void doAfterCancel() throws BaseException {
+		super.doAfterCancel();
+		if(!conversation.isTransient())
+			conversation.end();
+	}
+	
+	public void addCancerOtrasPartes(ActionEvent e){
+		if(this.cancerOtrasPartes.getParteCuerpo() != null && !this.cancerOtrasPartes.getParteCuerpo().isEmpty()){
+			this.cancerOtrasPartes.setPaciente(getModel());
+			this.cancerList.add(cancerOtrasPartes);
+			this.cancerOtrasPartes = new CancerOtrasPartes();
+		}
+	}
+	
+	public void addPreviaEnfermedad(ActionEvent e){
+		if(this.previaEnfermedad.getIllness() != null && !this.previaEnfermedad.getIllness().isEmpty()){
+			this.previaEnfermedad.setPaciente(getModel());
+			this.prevEnfList.add(previaEnfermedad);
+			this.previaEnfermedad = new PreviaEnfermedad();
+		}
+	}
+	
+	public void addParienteCancerMama(ActionEvent e){
+		if(this.parienteCancerMama.getRelative() != null && !this.parienteCancerMama.getRelative().isEmpty()){
+			this.parienteCancerMama.setPaciente(getModel());
+			this.parienteList.add(parienteCancerMama);
+			this.parienteCancerMama = new ParienteCancerMama();
+		}
+	}
+	
+	public void addSintomaCancerMama(ActionEvent e){
+		if(this.sintomaCancerMama.getSintomaNombre() != null && !this.sintomaCancerMama.getSintomaNombre().isEmpty()){
+			this.sintomaCancerMama.setPaciente(getModel());
+			this.sintomaList.add(sintomaCancerMama);
+			this.sintomaCancerMama = new SintomaCancerMama();
+		}
+	}
 	
 	public String navigate(){
 		return "home";
--- ExpedienteWeb/src/main/java/com/isesalud/model/Paciente.java
+++ ExpedienteWeb/src/main/java/com/isesalud/model/Paciente.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -40,326 +41,326 @@ public class Paciente extends BaseModel {
 	 */
 	private static final long serialVersionUID = 2325369627028054544L;
 	
-@Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
-@NotNull
-@Column(name="id",nullable = false,unique=true)
-private Long id;
-
-@Column(name="lastName", nullable = false, length = 100)
-@Length(max=100)
-@NotNull
-private String lastName;
-
-@Column(name="maternalLastName", nullable = true, length = 100)
-@Length(max=100)
-private String maternalLastName;
-
-@Column(name="name",nullable = false, length = 100)
-@NotNull
-@Length(max=100)
-private String name;
-
-
-@Column(name="dateOfBirth", nullable = false)
-@NotNull
-@Temporal(TemporalType.DATE)
-private Date dateofBirth;
-
-@Column(name="phoneNumber", nullable = true, length = 20)
-@Length(max=20)
-private String phoneNumber;
-
-@Column(name="address", nullable = true, length = 300)
-@Length(max=300)
-private String address;
-
-@Column(name="sex", nullable = false)
-@NotNull
-private char sex;
-
-@Column(name="seguroPopular", nullable = true)
-private String seguroPopular;
-
-@Column(name="dateCreated", nullable=false)
-@NotNull
-@Temporal(TemporalType.DATE)
-private Date dateCreated;
-
-@Column(name="timeCreated",nullable = false)
-@NotNull
-@Temporal(TemporalType.TIME)
-private Date timeCreated;
-
-@Column(name="ageMenstruation", nullable = false)
-@NotNull
-private Integer ageMenstruation;
-
-@Column(name="ageFirstChild", nullable = false)
-@NotNull
-private Integer ageFirstChild;
-
-@Column(name="BreastFeedChild", nullable = false)
-@NotNull
-private Boolean BreastFeedChild;
-
-@Column (name="yearsSinceSurgery", nullable = false)
-@NotNull
-private Integer yearsSinceSurgery;
-
-@Column (name = "ageMenopause",nullable = false)
-@NotNull
-private Integer ageMenopause;
-
-@Column (name = "ageMenopauseHormones", nullable = false)
-@NotNull
-private Integer ageMenopauseHormones;
-
-@Column(name = "DateLastMG", nullable = false)
-@NotNull
-@Temporal(TemporalType.DATE)
-private Date dateLastMG;
-
-@Column (name = "observations", nullable = true, length = 300)
-@Length(max = 300)
-private String observations;
-
-@ManyToOne(fetch=FetchType.LAZY)
-@JoinColumn(name="personal", nullable=false)
-private Personal personal;
-
-@ManyToOne(fetch=FetchType.LAZY)
-@JoinColumn(name="municipio",nullable=false)
-private Municipio municipio;
-
-@ManyToOne(fetch=FetchType.LAZY)
-@JoinColumn(name="entidad", nullable = false)
-private Entidad entidad;
-
-@ManyToOne(fetch=FetchType.LAZY)
-@JoinColumn(name="unidadmedica", nullable = false)
-private Unidadmedica unidadmedica;
-
-@OneToMany(fetch=FetchType.LAZY, mappedBy="paciente")
-private List<ParienteCancerMama> parientescancermama = new ArrayList<ParienteCancerMama>();
-
-@OneToMany(fetch=FetchType.LAZY, mappedBy="paciente")
-private List<CancerOtrasPartes> canceresOtraPartes = new ArrayList<CancerOtrasPartes>();
-
-@OneToMany(fetch=FetchType.LAZY, mappedBy="paciente")
-private List<PreviaEnfermedad> previasenfermedades = new ArrayList<PreviaEnfermedad>();
-
-@OneToMany(fetch=FetchType.LAZY, mappedBy="paciente")
-private List<SintomaCancerMama> SintomasCancerMama = new ArrayList<SintomaCancerMama>();
- 
-@OneToMany(fetch=FetchType.LAZY,mappedBy="paciente")
-private List<Cita> citas = new ArrayList<Cita>();
-
-public Paciente() {
-	this.id = new Long(0L);
-}
-
-public Paciente(String lastName, String maternalLastName, String name,
-		Date dateofBirth, String phoneNumber, String address, char sex,
-		Personal personal, Municipio municipio, Entidad entidad,
-		Unidadmedica unidadmedica) {
-	this.id = new Long(0L);
-	this.lastName = lastName;
-	this.maternalLastName = maternalLastName;
-	this.name = name;
-	this.dateofBirth = dateofBirth;
-	this.phoneNumber = phoneNumber;
-	this.address = address;
-	this.sex = sex;
-	this.personal = personal;
-	this.municipio = municipio;
-	this.entidad = entidad;
-	this.unidadmedica = unidadmedica;
-}
-
-public List<Cita> getCitas() {
-	return citas;
-}
-
-public void setCitas(List<Cita> citas) {
-	this.citas = citas;
-}
-
-public List<SintomaCancerMama> getSintomasCancerMama() {
-	return SintomasCancerMama;
-}
-public void setSintomasCancerMama(List<SintomaCancerMama> sintomasCancerMama) {
-	SintomasCancerMama = sintomasCancerMama;
-}
-
-public List<CancerOtrasPartes> getCanceresOtraPartes() {
-	return canceresOtraPartes;
-}
-
-public void setCanceresOtraPartes(List<CancerOtrasPartes> canceresOtraPartes) {
-	this.canceresOtraPartes = canceresOtraPartes;
-}
-
-public List<PreviaEnfermedad> getPreviasenfermedades() {
-	return previasenfermedades;
-}
-
-public void setPreviasenfermedades(List<PreviaEnfermedad> previasenfermedades) {
-	this.previasenfermedades = previasenfermedades;
-}
-
-public Entidad getEntidad() {
-	return entidad;
-}
-
-public void setEntidad(Entidad entidad) {
-	this.entidad = entidad;
-}
-
-public Unidadmedica getUnidadmedica() {
-	return unidadmedica;
-}
- 
-public void setUnidadmedica(Unidadmedica unidadmedica) {
-	this.unidadmedica = unidadmedica;
-}
-
-public Municipio getMunicipio() {
-	return municipio;
-}
-
-public void setMunicipio(Municipio municipio) {
-	this.municipio = municipio;
-}
-
-public List<ParienteCancerMama> getParientescancermama() {
-	return parientescancermama;
-}
-public void setParientescancermama(List<ParienteCancerMama> parientescancermama) {
-	this.parientescancermama = parientescancermama;
-}
-
-public String getAddress() {
-	return address;
-}
-public void setAddress(String address) {
-	this.address = address;
-}
-public Integer getAgeFirstChild() {
-	return ageFirstChild;
-}
-
-public void setAgeFirstChild(Integer ageFirstChild) {
-	this.ageFirstChild = ageFirstChild;
-}
-public Integer getAgeMenopause() {
-	return ageMenopause;
-}
-public void setAgeMenopause(Integer ageMenopause) {
-	this.ageMenopause = ageMenopause;
-}
-public Integer getAgeMenopauseHormones() {
-	return ageMenopauseHormones;
-}
-public void setAgeMenopauseHormones(Integer ageMenopauseHormones) {
-	this.ageMenopauseHormones = ageMenopauseHormones;
-}
-public Integer getAgeMenstruation() {
-	return ageMenstruation;
-}
-public void setAgeMenstruation(Integer ageMenstruation) {
-	this.ageMenstruation = ageMenstruation;
-}
-public Boolean getBreastFeedChild() {
-	return BreastFeedChild;
-}
-public void setBreastFeedChild(Boolean breastFeedChild) {
-	BreastFeedChild = breastFeedChild;
-}
-public Date getDateCreated() {
-	return dateCreated;
-}
-public void setDateCreated(Date dateCreated) {
-	this.dateCreated = dateCreated;
-}
-public Date getDateLastMG() {
-	return dateLastMG;
-}
-public void setDateLastMG(Date dateLastMG) {
-	this.dateLastMG = dateLastMG;
-}
-public Date getDateofBirth() {
-	return dateofBirth;
-}
-public void setDateofBirth(Date dateofBirth) {
-	this.dateofBirth = dateofBirth;
-}
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public String getLastName() {
-	return lastName;
-}
-public void setLastName(String lastName) {
-	this.lastName = lastName;
-}
-public String getMaternalLastName() {
-	return maternalLastName;
-}
-public void setMaternalLastName(String maternalLastName) {
-	this.maternalLastName = maternalLastName;
-}
-public String getObservations() {
-	return observations;
-}
-public void setObservations(String observations) {
-	this.observations = observations;
-}
-public String getPhoneNumber() {
-	return phoneNumber;
-}
-public void setPhoneNumber(String phoneNumber) {
-	this.phoneNumber = phoneNumber;
-}
-public String getSeguroPopular() {
-	return seguroPopular;
-}
-public void setSeguroPopular(String seguroPopular) {
-	this.seguroPopular = seguroPopular;
-}
-public char getSex() {
-	return sex;
-}
-public void setSex(char sex) {
-	this.sex = sex;
-}
-public Date getTimeCreated() {
-	return timeCreated;
-}
-public void setTimeCreated(Date timeCreated) {
-	this.timeCreated = timeCreated;
-}
-public Integer getYearsSinceSurgery() {
-	return yearsSinceSurgery;
-}
-public void setYearsSinceSurgery(Integer yearsSinceSurgery) {
-	this.yearsSinceSurgery = yearsSinceSurgery;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public Personal getPersonal() {
-	return personal;
-}
-public void setPersonal(Personal personal) {
-	this.personal = personal;
-}
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@NotNull
+	@Column(name="id",nullable = false,unique=true)
+	private Long id;
+	
+	@Column(name="lastName", nullable = false, length = 100)
+	@Length(max=100)
+	@NotNull
+	private String lastName;
+	
+	@Column(name="maternalLastName", nullable = true, length = 100)
+	@Length(max=100)
+	private String maternalLastName;
+	
+	@Column(name="name",nullable = false, length = 100)
+	@NotNull
+	@Length(max=100)
+	private String name;
+	
+	
+	@Column(name="dateOfBirth", nullable = false)
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	private Date dateofBirth;
+	
+	@Column(name="phoneNumber", nullable = true, length = 20)
+	@Length(max=20)
+	private String phoneNumber;
+	
+	@Column(name="address", nullable = true, length = 300)
+	@Length(max=300)
+	private String address;
+	
+	@Column(name="sex", nullable = false)
+	@NotNull
+	private char sex;
+	
+	@Column(name="seguroPopular", nullable = true)
+	private String seguroPopular;
+	
+	@Column(name="dateCreated", nullable=false)
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	private Date dateCreated;
+	
+	@Column(name="timeCreated",nullable = false)
+	@NotNull
+	@Temporal(TemporalType.TIME)
+	private Date timeCreated;
+	
+	@Column(name="ageMenstruation", nullable = false)
+	@NotNull
+	private Integer ageMenstruation;
+	
+	@Column(name="ageFirstChild", nullable = false)
+	@NotNull
+	private Integer ageFirstChild;
+	
+	@Column(name="BreastFeedChild", nullable = false)
+	@NotNull
+	private Boolean BreastFeedChild;
+	
+	@Column (name="yearsSinceSurgery", nullable = false)
+	@NotNull
+	private Integer yearsSinceSurgery;
+	
+	@Column (name = "ageMenopause",nullable = false)
+	@NotNull
+	private Integer ageMenopause;
+	
+	@Column (name = "ageMenopauseHormones", nullable = false)
+	@NotNull
+	private Integer ageMenopauseHormones;
+	
+	@Column(name = "DateLastMG", nullable = false)
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	private Date dateLastMG;
+	
+	@Column (name = "observations", nullable = true, length = 300)
+	@Length(max = 300)
+	private String observations;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="personal", nullable=false)
+	private Personal personal;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="municipio",nullable=false)
+	private Municipio municipio;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="entidad", nullable = false)
+	private Entidad entidad;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="unidadmedica", nullable = false)
+	private Unidadmedica unidadmedica;
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="paciente")
+	private List<ParienteCancerMama> parientescancermama = new ArrayList<ParienteCancerMama>();
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="paciente")
+	private List<CancerOtrasPartes> canceresOtraPartes = new ArrayList<CancerOtrasPartes>();
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="paciente")
+	private List<PreviaEnfermedad> previasenfermedades = new ArrayList<PreviaEnfermedad>();
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="paciente")
+	private List<SintomaCancerMama> SintomasCancerMama = new ArrayList<SintomaCancerMama>();
+	 
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY,mappedBy="paciente")
+	private List<Cita> citas = new ArrayList<Cita>();
+	
+	public Paciente() {
+		this.id = new Long(0L);
+	}
+	
+	public Paciente(String lastName, String maternalLastName, String name,
+			Date dateofBirth, String phoneNumber, String address, char sex,
+			Personal personal, Municipio municipio, Entidad entidad,
+			Unidadmedica unidadmedica) {
+		this.id = new Long(0L);
+		this.lastName = lastName;
+		this.maternalLastName = maternalLastName;
+		this.name = name;
+		this.dateofBirth = dateofBirth;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		this.sex = sex;
+		this.personal = personal;
+		this.municipio = municipio;
+		this.entidad = entidad;
+		this.unidadmedica = unidadmedica;
+	}
+	
+	public List<Cita> getCitas() {
+		return citas;
+	}
+	
+	public void setCitas(List<Cita> citas) {
+		this.citas = citas;
+	}
+	
+	public List<SintomaCancerMama> getSintomasCancerMama() {
+		return SintomasCancerMama;
+	}
+	public void setSintomasCancerMama(List<SintomaCancerMama> sintomasCancerMama) {
+		SintomasCancerMama = sintomasCancerMama;
+	}
+	
+	public List<CancerOtrasPartes> getCanceresOtraPartes() {
+		return canceresOtraPartes;
+	}
+	
+	public void setCanceresOtraPartes(List<CancerOtrasPartes> canceresOtraPartes) {
+		this.canceresOtraPartes = canceresOtraPartes;
+	}
+	
+	public List<PreviaEnfermedad> getPreviasenfermedades() {
+		return previasenfermedades;
+	}
+	
+	public void setPreviasenfermedades(List<PreviaEnfermedad> previasenfermedades) {
+		this.previasenfermedades = previasenfermedades;
+	}
+	
+	public Entidad getEntidad() {
+		return entidad;
+	}
+	
+	public void setEntidad(Entidad entidad) {
+		this.entidad = entidad;
+	}
+	
+	public Unidadmedica getUnidadmedica() {
+		return unidadmedica;
+	}
+	 
+	public void setUnidadmedica(Unidadmedica unidadmedica) {
+		this.unidadmedica = unidadmedica;
+	}
+	
+	public Municipio getMunicipio() {
+		return municipio;
+	}
+	
+	public void setMunicipio(Municipio municipio) {
+		this.municipio = municipio;
+	}
+	
+	public List<ParienteCancerMama> getParientescancermama() {
+		return parientescancermama;
+	}
+	public void setParientescancermama(List<ParienteCancerMama> parientescancermama) {
+		this.parientescancermama = parientescancermama;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Integer getAgeFirstChild() {
+		return ageFirstChild;
+	}
+	
+	public void setAgeFirstChild(Integer ageFirstChild) {
+		this.ageFirstChild = ageFirstChild;
+	}
+	public Integer getAgeMenopause() {
+		return ageMenopause;
+	}
+	public void setAgeMenopause(Integer ageMenopause) {
+		this.ageMenopause = ageMenopause;
+	}
+	public Integer getAgeMenopauseHormones() {
+		return ageMenopauseHormones;
+	}
+	public void setAgeMenopauseHormones(Integer ageMenopauseHormones) {
+		this.ageMenopauseHormones = ageMenopauseHormones;
+	}
+	public Integer getAgeMenstruation() {
+		return ageMenstruation;
+	}
+	public void setAgeMenstruation(Integer ageMenstruation) {
+		this.ageMenstruation = ageMenstruation;
+	}
+	public Boolean getBreastFeedChild() {
+		return BreastFeedChild;
+	}
+	public void setBreastFeedChild(Boolean breastFeedChild) {
+		BreastFeedChild = breastFeedChild;
+	}
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public Date getDateLastMG() {
+		return dateLastMG;
+	}
+	public void setDateLastMG(Date dateLastMG) {
+		this.dateLastMG = dateLastMG;
+	}
+	public Date getDateofBirth() {
+		return dateofBirth;
+	}
+	public void setDateofBirth(Date dateofBirth) {
+		this.dateofBirth = dateofBirth;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMaternalLastName() {
+		return maternalLastName;
+	}
+	public void setMaternalLastName(String maternalLastName) {
+		this.maternalLastName = maternalLastName;
+	}
+	public String getObservations() {
+		return observations;
+	}
+	public void setObservations(String observations) {
+		this.observations = observations;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getSeguroPopular() {
+		return seguroPopular;
+	}
+	public void setSeguroPopular(String seguroPopular) {
+		this.seguroPopular = seguroPopular;
+	}
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	public Date getTimeCreated() {
+		return timeCreated;
+	}
+	public void setTimeCreated(Date timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+	public Integer getYearsSinceSurgery() {
+		return yearsSinceSurgery;
+	}
+	public void setYearsSinceSurgery(Integer yearsSinceSurgery) {
+		this.yearsSinceSurgery = yearsSinceSurgery;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Personal getPersonal() {
+		return personal;
+	}
+	public void setPersonal(Personal personal) {
+		this.personal = personal;
+	}
 }
 
 
