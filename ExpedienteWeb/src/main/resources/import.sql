a7b1576a8d71274a45cf74c77239449dfad2b076
ari <arigsela@gmail.com>
2013-03-13 14:32:00 -0700

    se agrego el campo de telefonomovil al entity de paciente
    Se modifico la pagina de home y pacienteentradas para usar el campo de
    telefono movil
    
    se agrego un telefono movil al paciente en import.sql

--- ExpedienteWeb/src/main/resources/import.sql
+++ ExpedienteWeb/src/main/resources/import.sql
@@ -5,7 +5,7 @@ insert into puesto (id, name, description) values (1, 'Capturista', 'El lacky');
 insert into personal (id, lastname, maternallastname, name, puesto) values (1, 'Sela', 'Muñoz', 'Ari', 1);
 insert into unidadmedica (id, name, description, code) values (1, 'Hosptial General', 'Some place', 0);
 insert into unidadmedica (id, name, description, code) values (2, 'Centro', 'Some place', 1);
-insert into paciente (id, lastName, maternalLastName, name, dateofBirth, phoneNumber, address, sex, seguroPopular, dateCreated, timeCreated, ageMenstruation, ageFirstChild, BreastFeedChild, yearsSinceSurgery, ageMenopause, ageMenopauseHormones, dateLastMG, observations, municipio, entidad, unidadmedica) values (1, 'Perez', 'Mendoza', 'Ari', '1980-02-16', '123456', 'Calle bleh', 'M', '0', '2013-02-11', '12:00:00', 0, 0, 0, 0, 0, 0, '2012-01-01', 'None', 1, 1, 1);
+insert into paciente (id, lastName, maternalLastName, name, dateofBirth, phoneNumber,phoneNumberMovil, address, sex, seguroPopular, dateCreated, timeCreated, ageMenstruation, ageFirstChild, BreastFeedChild, yearsSinceSurgery, ageMenopause, ageMenopauseHormones, dateLastMG, observations, municipio, entidad, unidadmedica) values (1, 'Perez', 'Mendoza', 'Ari', '1980-02-16', '123456','6641223322', 'Calle bleh', 'M', '0', '2013-02-11', '12:00:00', 0, 0, 0, 0, 0, 0, '2012-01-01', 'None', 1, 1, 1);
 insert into tipocita (id,description) values (1,'Radiologia')
 insert into tipocita (id,description) values (2,'Oncologia')
 insert into tipoestudiocita (id,name,notes,tipocita) value (1,'mastografia tipo A','Tiene que llevar su comprobante',1)
