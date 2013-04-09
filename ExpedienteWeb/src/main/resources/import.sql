e132b4e50413c849bea491a413cb55e787455332
ari <arigsela@gmail.com>
2013-04-09 13:41:19 -0700

    Se cambiaron los archivos de persistence e import

--- ExpedienteWeb/src/main/resources/import.sql
+++ ExpedienteWeb/src/main/resources/import.sql
@@ -6,7 +6,7 @@ insert into puesto (id, name, description) values (1, 'Capturista', 'El lacky');
 insert into personal (id, lastname, maternallastname, name, puesto) values (1, 'Sela', 'Muñoz', 'Ari', 1);
 insert into unidadmedica (id, name, description, municipio, code) values (1, 'Hospital General', 'Hospital General de Tijuana',1, 0);
 insert into unidadmedica (id, name, description,municipio, code) values (2, 'PACS 1', 'Unidad movil de Tijuana 1',2, 1);
-insert into paciente (id, lastName, maternalLastName, name, dateofBirth, phoneNumber,phoneNumberMovil, aceptamensajes,address, sex, seguroPopular, dateCreated, timeCreated, ageMenstruation, ageFirstChild, BreastFeedChild, yearsSinceSurgery, typeofsurgery, ageMenopause, ageMenopauseHormones, dateLastMG, observations, municipio, entidad, unidadmedica) values (1, 'Perez', 'Mendoza', 'Ari', '1980-02-16', '123456','6641223322','1', 'Calle del rosario #13190', 'M', '0', '2013-02-11', '12:00:00', 0, 0, 0, 0,'ninguna', 0, 0, '2012-01-01', 'None', 1, 1, 1);
+insert into paciente (id, lastName, maternalLastName, name, dateofBirth, phoneNumber,phoneNumberMovil, aceptamensajes,address, sex, seguroPopular, dateCreated, timeCreated, ageMenstruation, ageFirstChild, BreastFeedChild, yearsSinceSurgery, typeofsurgery, ageMenopause, ageMenopauseHormones, dateLastMG, observations, municipio, entidad, unidadmedica) values (1, 'Perez', 'Mendoza', 'Ari', '1980-02-16', '123456','6642865641','1', 'Calle del rosario #13190', 'M', '0', '2013-02-11', '12:00:00', 0, 0, 0, 0,'ninguna', 0, 0, '2012-01-01', 'None', 1, 1, 1);
 insert into tipocita (id,description) values (1,'Radiologia');
 insert into tipocita (id,description) values (2,'Oncologia');
 insert into tipoestudiocita (id,name,notes,tipocita) value (1,'TAM. INVITACION ORGANIZADA','Tiene que llevar su comprobante',1);
@@ -19,7 +19,7 @@ insert into tipoestudiocita (id,name,notes,tipocita) value (7,'oncologia tipo A'
 insert into statuscita (id,description) values (1,'Pendiente');
 insert into statuscita (id,description) values (2,'Atendida');
 insert into cita (id,date,time,statusCita,tipoEstudioCita,paciente,enviadosms) values (1,'2013-03-22','12:10:00',1,1,1,0);
-insert into systemsettings (id,messagesenabled) values (1,0);
+insert into systemsettings (id,messagesenabled) values (1,1);
 insert into user (username, password, name, fName, mName, municipio, enabled) values ('test', 'YEr9AHbeWymox3oz/OKqOhjkzOr75H2sQz/ZQ2OwhRc=', 'John', 'Marston', '', 1, 1);
 insert into user (username, password, name, fName, mName, municipio, enabled) values ('test2', 'YEr9AHbeWymox3oz/OKqOhjkzOr75H2sQz/ZQ2OwhRc=', 'Tifa', 'Lockheart', '', 2, 1);
 insert into role (role, description) values ('Admin', 'Administrador');
