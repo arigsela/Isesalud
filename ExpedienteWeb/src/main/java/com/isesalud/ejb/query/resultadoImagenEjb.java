07813a2eeb5797322edfafe5be14774fb98f5267
ari <arigsela@gmail.com>
2013-04-11 11:53:09 -0700

    Modificaciones a Radiologia

--- ExpedienteWeb/src/main/java/com/isesalud/ejb/query/resultadoImagenEjb.java
+++ ExpedienteWeb/src/main/java/com/isesalud/ejb/query/resultadoImagenEjb.java
@@ -3,14 +3,17 @@
  */
 package com.isesalud.ejb.query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import com.isesalud.model.resultadoImagen;
+import com.isesalud.model.resultadoImagen_;
 import com.isesalud.support.components.BaseManagerEJB;
 
 /**
@@ -20,6 +23,7 @@ import com.isesalud.support.components.BaseManagerEJB;
 @Stateless
 @LocalBean
 public class resultadoImagenEjb extends BaseManagerEJB<resultadoImagen>{
+	
 	public List<resultadoImagen> getAllResultados(){
 		CriteriaBuilder builder = em.getCriteriaBuilder();
 		CriteriaQuery<resultadoImagen> query = builder.createQuery(getModelClass());
@@ -28,6 +32,21 @@ public class resultadoImagenEjb extends BaseManagerEJB<resultadoImagen>{
 		return getList(query);
 	}
 
+	public List<resultadoImagen> getResultado(resultadoImagen params){
+		List<resultadoImagen> model =null;
+		List<Predicate> predicates = new ArrayList<Predicate>();
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<resultadoImagen> query = builder.createQuery(getModelClass());
+		Root<resultadoImagen> root = query.from(getModelClass());
+		Predicate paciente = builder.equal(root.get(resultadoImagen_.paciente), params.getPaciente());
+		predicates.add(paciente);
+		Predicate and = builder.and(predicates.toArray(new Predicate[]{}));
+		query.select(root).where(and);
+
+		model = getList(query);
+		
+		return model;
+	}
 	@Override
 	public Class<resultadoImagen> getModelClass() {
 		return resultadoImagen.class;
