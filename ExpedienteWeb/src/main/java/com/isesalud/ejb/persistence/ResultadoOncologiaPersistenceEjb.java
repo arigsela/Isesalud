fdf5c67fcac47f121c1f545c5ce62a7b93eed349
Jesus Espinoza Hernandez <chuyespinoza@gmail.com>
2013-04-22 19:55:12 -0700

    Added missing annotations.

--- ExpedienteWeb/src/main/java/com/isesalud/ejb/persistence/ResultadoOncologiaPersistenceEjb.java
+++ ExpedienteWeb/src/main/java/com/isesalud/ejb/persistence/ResultadoOncologiaPersistenceEjb.java
@@ -3,6 +3,9 @@
  */
 package com.isesalud.ejb.persistence;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
 import com.isesalud.model.ResultadoOncologia;
 import com.isesalud.support.components.BasePersistenceManagerEJB;
 
@@ -10,6 +13,8 @@ import com.isesalud.support.components.BasePersistenceManagerEJB;
  * @author ari
  *
  */
+@Stateless
+@LocalBean
 public class ResultadoOncologiaPersistenceEjb extends BasePersistenceManagerEJB<ResultadoOncologia>{
 
 	@Override
