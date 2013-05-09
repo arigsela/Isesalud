743628cf7b50107470a72a8f19278f24a84ca032
Jesus Espinoza Hernandez <chuyespinoza@gmail.com>
2013-05-09 11:10:42 -0700

    Revert "ignora este comit"
    This reverts commit cd204942892aa6efc93f988882327b05d2dd6b96.

--- ExpedienteWeb/src/main/resources/import.sql
+++ ExpedienteWeb/src/main/resources/import.sql
@@ -6,6 +6,9 @@ insert into municipio (id, name, description) values (1, 'Tijuana', 'Municipio d
 insert into municipio (id, name, description) values (2, 'Mexicali', 'Municipio de Mexicali');
 insert into puesto (id, name, description) values (1, 'Capturista', 'El lacky');
 insert into personal (id, lastname, maternallastname, name, puesto,gabinete) values (1, 'Sela', 'Muñoz', 'Ari', 1,1);
+insert into unidadmedica (id, name, description, municipio, code) values (1, 'Hospital General', 'Hospital General de Tijuana',1, 0);
+insert into unidadmedica (id, name, description,municipio, code) values (2, 'PACS 1', 'Unidad movil de Tijuana 1',2, 1);
+insert into paciente (id, lastName, maternalLastName, name, dateofBirth, phoneNumber,phoneNumberMovil, aceptamensajes,address, sex, seguroPopular, dateCreated, timeCreated, ageMenstruation, ageFirstChild, BreastFeedChild, yearsSinceSurgery, typeofsurgery, ageMenopause, ageMenopauseHormones, dateLastMG, observations, municipio, entidad, entidadNacimiento, unidadmedica) values (1, 'Perez', 'Mendoza', 'Ari', '1980-02-16', '123456','6642865641','1', 'Calle del rosario #13190', 'M', '0', '2013-02-11', '12:00:00', 0, 0, 0, 0,'ninguna', 0, 0, '2012-01-01', 'None', 1, 1, 1,1);
 insert into department (id,description) values (1,'Radiologia');
 insert into department (id,description) values (2,'Oncologia');
 insert into modality (id,description,name,department) values (1,"MASTOGRAFIAS","MM",1)
@@ -30,5 +33,8 @@ insert into role (role, description) values ('Tech', 'Tecnico');
 insert into role (role, description) values ('Doctor', 'Doctor');
 insert into user_has_role (User_id, Role_id) values (1,1);
 insert into tiporesultado (id,description,name) values (1,'BIRADS 0 INSEGURO','B0');
+insert into motivoInadecuada (id,description,name) values (1,'TOMADA ADECUADAMENTE','ADECUADA');
+insert into resultadoImagen (id,adequate,dateNotified,dateInterpreted,daterealized,signs,gabineterealizado,gabineteinter,MotivoInadecuada,paciente,personalrealizado,personalinter,study,TipoResultado) values (1,1,'2013-03-31','2013-03-31','2013-03-29','Ninguna',1,1,1,1,1,1,1,1);
+insert into hallazgos (id,name,side,resultadoImagen) values (1,'nada','IZQ',1);
 insert into motivoReferencia (id, name, description) values (1, 'mot1', 'motivo 1');
 insert into motivoReferencia (id, name, description) values (2, 'mot2', 'motivo 2');
