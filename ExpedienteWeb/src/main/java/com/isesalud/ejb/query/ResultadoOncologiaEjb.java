5736fda068f4502cf044a8bd0c48ec2e8dee1159
ari <arigsela@gmail.com>
2013-05-21 13:09:39 -0700

    Arregle una asociacion fallida con un entity modificado de nombre para onco

--- ExpedienteWeb/src/main/java/com/isesalud/ejb/query/ResultadoOncologiaEjb.java
+++ ExpedienteWeb/src/main/java/com/isesalud/ejb/query/ResultadoOncologiaEjb.java
@@ -8,7 +8,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import com.isesalud.model.HistoriaClinicaOnco;
-import com.isesalud.model.ResultadoOncologia_;
+import com.isesalud.model.HistoriaClinicaOnco_;
 import com.isesalud.support.components.BaseManagerEJB;
 
 @Stateless
@@ -27,7 +27,7 @@ public class ResultadoOncologiaEjb extends BaseManagerEJB<HistoriaClinicaOnco>{
 		CriteriaBuilder builder = em.getCriteriaBuilder();
 		CriteriaQuery<HistoriaClinicaOnco> query = builder.createQuery(getModelClass());
 		Root<HistoriaClinicaOnco> root = query.from(getModelClass());
-		query.select(root).where(builder.equal(root.get(ResultadoOncologia_.paciente), params.getPaciente()));
+		query.select(root).where(builder.equal(root.get(HistoriaClinicaOnco_.paciente), params.getPaciente()));
 		return getList(query);
 	}
 
